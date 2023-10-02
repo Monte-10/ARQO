d7751dce1361b10fbd0523d90c5144ac90960350
Monte-10 <alejandro.monterrubio@estudiante.uam.es>
2023-10-02 10:53:31 +0200

    Ejercicio 2 terminado

--- P1/Ejercicio 2/sorted_list.asm
+++ P1/Ejercicio 2/sorted_list.asm
@@ -15,18 +15,18 @@ copy_loop:
     sw a3, 0(a1)           # Guardamos el valor en la lista ordenada
     addi a0, a0, 4         # Avanzamos a la siguiente posición en la lista original
     addi a1, a1, 4         # Avanzamos a la siguiente posición en la lista ordenada
-    addi a2, a2, -1        # Decrementamos el contador
+    addi a2, a2, -1        # Reducimos en 1 el contador
     bnez a2, copy_loop     # Si el contador no es cero, repetimos el bucle
 
-    # Realizamos la ordenación de burbuja
-    li a4, 9               # Contador externo para el bucle de burbuja
+    # Realizamos BubbleSort ascendente
+    li a4, 9               # Contador para el bucle externo
 outer_loop:
-    li a5, 0               # Contador interno para el bucle de burbuja
+    li a5, 0               # Contador para el bucle interno
     la a1, sorted_list     # Reseteamos la dirección inicial de la lista ordenada para el bucle interno
 inner_loop:
     lw a6, 0(a1)           # Cargamos el valor actual en a6
     lw a7, 4(a1)           # Cargamos el siguiente valor en a7
-    ble a6, a7, skip_swap  # Si el valor actual es menor o igual al siguiente, saltamos al intercambio
+    blt a6, a7, skip_swap  # Si el valor actual es menor al siguiente, saltamos al intercambio
 
     # Intercambiamos los valores
     sw a7, 0(a1)
@@ -34,10 +34,10 @@ inner_loop:
 
 skip_swap:
     addi a1, a1, 4         # Avanzamos a la siguiente posición en la lista ordenada
-    addi a5, a5, 1         # Incrementamos el contador interno
+    addi a5, a5, 1         # Incrementamos en 1 el contador interno
     blt a5, a4, inner_loop # Si el contador interno es menor que el contador externo, repetimos el bucle interno
 
-    addi a4, a4, -1        # Decrementamos el contador externo
+    addi a4, a4, -1        # Reducimos en 1 el contador externo
     bnez a4, outer_loop    # Si el contador externo no es cero, repetimos el bucle externo
 
     # Terminamos el programa
