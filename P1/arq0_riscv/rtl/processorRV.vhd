efa4f21dca3f05fa48f36f0500f89a5295768e85
Florentino_Garcia <florentino.garcia.aznar@gmail.com>
2023-10-06 12:39:32 +0200

    ++

--- P1/arq0_riscv/rtl/processorRV.vhd
+++ P1/arq0_riscv/rtl/processorRV.vhd
@@ -168,29 +168,6 @@ architecture rtl of processorRV is
   signal dataIn_WB : std_logic_vector(31 downto 0);
   signal enable_MEM_WB: std_logic;
 
-  --Unidad de forwarding
-  signal Rs1_EX: std_logic_vector(4 downto 0);
-  signal Rs2_EX: std_logic_vector(4 downto 0);
-  signal ForwardA: std_logic_vector(1 downto 0);
-  signal ForwardB: std_logic_vector(1 downto 0);
-  signal auxsignalOP2: std_logic_vector(31 downto 0);
-  signal auxsignalOP1: std_logic_vector(31 downto 0);
-
-  --hazard detection unit
-  signal Ctrl_mux_hazard: std_logic;
-  signal Ctrl_Branch_MUX : std_logic;
-  signal Ctrl_ResSrc_MUX : std_logic_vector(1 downto 0);
-  signal Ctrl_MemWrite_MUX : std_logic;
-  signal Ctrl_MemRead_MUX : std_logic;
-  signal Ctrl_PcLui_MUX : std_logic_vector(1 downto 0);
-  signal Ctrl_jal_MUX : std_logic;
-  signal Ctrl_jalr_MUX : std_logic;
-  signal Ctrl_RegWrite_MUX : std_logic;
-  signal Ctrl_ALUSrc_MUX : std_logic;
-  signal Ctrl_ALUOp_MUX : std_logic_vector(2 downto 0);
-  signal flush_IF_ID, flush_ID_EX, flush_EX_MEM : std_logic;
-
-
 begin
 
   PC_next <= Addr_Jump_dest when decision_Jump = '1' else PC_plus4;
@@ -210,18 +187,11 @@ begin
     IF_ID_Regs: process(clk,reset)
     begin
       if Reset = '1' then
-        PC_ID <= (others => '0');
-        Instruction_ID <= (others=>'0');
-      elsif rising_edge(clk) then
-        if enable_IF_ID = '1' and flush_IF_ID = '0' then
-          PC_ID <= PC_IF;
-          Instruction_ID <= Instruction_IF;
-
-        elsif flush_IF_ID = '1' then
-          PC_ID <= (others => '0');
-          Instruction_ID <= (others=>'0');
-        end if;
-        
+        PC_ID <= (others=>'0');
+        instruction_ID <=(others=>'0');
+      elsif rising_edge(clk) and enable_IF_ID='1' then
+        PC_ID<=PC_IF;
+        instruction_ID<=instruccion_IF;
       end if;
     end process;
 
@@ -231,12 +201,165 @@ begin
     instruction<=IDataIn;
 
   PC_plus4    <= PC_reg + 4;
-  IAddr       <= PC_reg;
   Funct3      <= instruction(14 downto 12); -- Campo "funct3" de la instruccion
   Funct7      <= instruction(31 downto 25); -- Campo "funct7" de la instruccion
   RD          <= Instruction(11 downto 7);
   RS1         <= Instruction(19 downto 15);
   RS2         <= Instruction(24 downto 20);
+
+-------ID/EX-----------------------------
+    ID_EX_Regs: process(clk,reset)
+    begin
+      if Reset = '1' then
+        PC_EX<=(others=>'0');
+        Funct3_EX<=(others=>'0');
+        Funct7_EX<=(others=>'0');
+        Inm_ext_EX<=(others => '0');
+        RD_EX<=(others => '0');
+
+        Ctrl_Branch_EX<= '0';
+        Ctrl_ResSrc_EX <= (others => '0');
+        Ctrl_MemWrite_EX<='0';
+        Ctrl_MemRead_EX<='0';
+        Ctrl_PcLui_EX<=(others => '0');
+        Ctrl_jalr_EX<='0'; 
+        Ctrl_RegWrite_EX<='0';
+
+        reg_RS1_EX <= (others => '0');
+        reg_RS2_EX <= (others => '0');
+
+        Ctrl_ALUSrc_EX<='0';
+        Ctrl_ALUOP_EX<=(others => '0');
+
+      elsif rising_edge(clk) and enable_ID_EX='1' then 
+        PC_EX<=PC_ID;
+        Funct3_EX<= Funct3_ID;  
+        Funct7_EX<= Funct7_ID;
+        Inm_ext_EX<=Inm_ext_ID;
+        RD_EX <= RD_ID;
+
+        Ctrl_Branch_EX <= Ctrl_Branch;
+        Ctrl_ResSrc_EX <= Ctrl_ResSrc;
+        Ctrl_MemWrite_EX<=Ctrl_MemWrite;
+        Ctrl_MemRead_EX<=Ctrl_MemRead;
+        Ctrl_PcLui_EX<=Ctrl_PcLui;
+        Ctrl_jalr_EX<=Ctrl_jalr; 
+        Ctrl_RegWrite_EX<=Ctrl_RegWrite;
+
+        reg_RS1_EX <= reg_RS1_ID;
+        reg_RS2_EX <= reg_RS2_ID;
+
+        Ctrl_ALUSrc_EX<=Ctrl_ALUSrc;
+        Ctrl_ALUOP_EX<=Ctrl_ALUOP;
+      end if;
+    end process;
+
+    enable_ID_EX<='1';
+
+    Addr_Branch_EX    <= PC_EX + Inm_ext_EX;
+    Addr_jalr_EX    <= reg_RS1_EX + Inm_ext_EX;
+
+    Alu_Op1_EX    <= PC_EX  when Ctrl_PcLui_EX = "00" else
+                  (others => '0')  when Ctrl_PcLui_EX = "01" else
+                  reg_RS1_EX;
+    Alu_Op2_EX   <= reg_RS2_EX when Ctrl_ALUSrc_EX = '0' else Inm_ext_EX;
+    
+    Addr_Jump_dest_EX <= Addr_jalr_EX   when Ctrl_jalr_EX = '1' else
+                      Addr_Branch_EX when Ctrl_Branch_EX='1' else
+                      (others =>'0');
+
+
+  ------EX/MEM-----------------------------------
+  EX_MEM_Regs: process(clk,reset)
+  begin
+    if Reset = '1' then
+      Addr_Jump_dest_MEM <= (others => '0');
+      Ctrl_Jalr_MEM<='0';
+      Ctrl_Branch_MEM<='0';
+
+      Alu_ZERO_MEM<= '0';
+      Alu_SIGN_MEM<='0';
+
+      Funct3_MEM<=(others => '0');
+
+      Ctrl_MemWrite_MEM<= '0';
+      Ctrl_MemRead_MEM<='0';
+
+      RD_MEM<=(others => '0');
+      reg_RS2_MEM<=(others => '0');
+      Alu_Res_MEM<=(others => '0');
+
+      Ctrl_ResSrc_MEM<= (others => '0');
+      Ctrl_RegWrite_MEM<='0';
+      
+    elsif rising_edge(clk) and enable_EX_MEM='1' then 
+      Addr_Jump_dest_MEM <= Addr_Jump_dest_EX;
+      Ctrl_Jalr_MEM<=Ctrl_jalr_EX;
+      Ctrl_Branch_MEM<=Ctrl_Branch_EX;
+
+      Alu_ZERO_MEM<= Alu_ZERO_EX;
+      Alu_SIGN_MEM<=Alu_SIGN_EX;
+
+      Funct3_MEM<=Funct3_EX;
+
+      Ctrl_MemWrite_MEM<= Ctrl_MemWrite_EX;
+      Ctrl_MemRead_MEM<=Ctrl_MemRead_EX;
+
+      RD_MEM<=RD_EX;
+      reg_RS2_MEM<=reg_RS2_EX;
+      Alu_Res_MEM<=Alu_Res_EX;
+
+      Ctrl_ResSrc_MEM<= Ctrl_ResSrc_EX;
+      Ctrl_RegWrite_MEM<=Ctrl_RegWrite_EX;
+
+    end if;
+  end process;
+
+  enable_EX_MEM<='1';
+
+  PC_next <= Addr_Jump_dest_MEM when decision_Jump_MEM = '1' else PC_plus4;
+
+  decision_Jump_MEM  <= Ctrl_Jalr_MEM or (Ctrl_Branch_MEM and branch_true_MEM);
+
+  branch_true_MEM    <= '1' when ( ((Funct3_MEM = BR_F3_BEQ) and (Alu_ZERO_MEM = '1')) or
+  ((Funct3_MEM = BR_F3_BNE) and (Alu_ZERO_MEM = '0')) or
+  ((Funct3_MEM = BR_F3_BLT) and (Alu_SIGN_MEM = '1')) or
+  ((Funct3_MEM = BR_F3_BGT) and (Alu_SIGN_MEM = '0')) ) else '0';
+
+  DWrEn<= Ctrl_MemWrite_MEM;
+  dRdEn<= Ctrl_MemRead_MEM;
+  DDataOut<= reg_RS2_MEM;
+  DAddr<= Alu_Res_MEM;
+  dataIn_MEM <= DDataIn;
+
+  ------MEM/WB-----------------------------------
+  MEM_WB_Regs: process(clk,reset)
+  begin
+    if Reset = '1' then
+      dataIn_WB<=(others => '0');
+      Alu_Res_WB<= (others => '0');
+      Ctrl_ResSrc_WB<= (others => '0');
+      Ctrl_RegWrite_WB<= '0';
+      RD_WB<=(others => '0');
+      
+    elsif rising_edge(clk) and enable_MEM_WB='1' then 
+      dataIn_WB<=dataIn_MEM;
+      Alu_Res_WB<= Alu_Res_MEM; 
+      Ctrl_ResSrc_WB<=Ctrl_ResSrc_MEM;
+      Ctrl_RegWrite_WB<=Ctrl_RegWrite_MEM;
+      RD_WB<=RD_MEM;
+    end if;
+  end process;
+
+  enable_MEM_WB<='1';
+
+  reg_RD_data_WB <= dataIn_WB when Ctrl_ResSrc_WB = "01" else
+                   PC_plus4   when Ctrl_ResSrc_WB = "10" else 
+                   Alu_Res_WB;
+
+
+-------------------------------------
+
 
   RegsRISCV : reg_bank
   port map (
